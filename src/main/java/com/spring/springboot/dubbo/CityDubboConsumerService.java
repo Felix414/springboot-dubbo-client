36a4d9e6ec69eeff6119b465d8c179c393c04259
Felix414 <ygplpr@126.com>
2019-06-20 12:07:50 +0800

    消费者项目代码

---
+++ src/main/java/com/spring/springboot/dubbo/CityDubboConsumerService.java
@@ -0,0 +1,40 @@
+package com.spring.springboot.dubbo;
+
+import org.springframework.stereotype.Component;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.spring.springboot.domain.City;
+
+/**
+ * 城市 Dubbo 服务消费者
+ *
+ * Created by zzg on 01/04/2017.
+ */
+@Component
+public class CityDubboConsumerService {
+ 
+	@Reference(version = "1.0.0")
+	CityDubboService cityDubboService;	
+ 
+	public CityDubboService getCityDubboService() {
+		return cityDubboService;
+	}
+	
+	public void setCityDubboService(CityDubboService cityDubboService) {
+		this.cityDubboService = cityDubboService;
+	}
+ 
+ 
+ 
+	public void printCity() {
+		String cityName = "南县";
+		if(cityDubboService == null){
+			System.out.println("cityDubboService 服务对象 为空");
+		}else{
+			System.out.println("cityDubboService 服务对象 不 为空");
+		}
+		City city = cityDubboService.findCityByName(cityName);
+		System.out.println(city.toString());
+	}
+ 
+}
